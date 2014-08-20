0884d9d6266cc36a68785a6877a178f81b834b23
rupert <rupert@2rmobile.com>
2014-08-21 07:26:04 +1000

    Add poi statistics in AdminHome

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -10,6 +10,10 @@ class Admin::DashboardController < Admin::ApplicationController
       @pois_no_location = Poi.where("latitude is null OR latitude = 0 OR longitude is null OR longitude = 0").count
       @pois_no_location_grouped = Poi.where("latitude is null OR latitude = 0 OR longitude is null OR longitude = 0").count(:group => :poi_type_name)
       @pois_no_description = Poi.where("description is null OR description = ''").count
+      @countries = Country.alphabetically
+
+      @poi_statistics = Poi.find_by_sql('SELECT country_name, poi_type_name, count(*) as total from pois GROUP BY country_name, poi_type_name')
+
       render 'index'
     end
   end
